2e1c2f5b3a5eee5c1fd3651ed37c03a78ea3306e
Chingyen <124688629+chingyen06@users.noreply.github.com>
2025-01-15 14:12:48 +0800

    Add files via upload

---
+++ 21/21.py
@@ -0,0 +1,90 @@
+def checkError(card):
+    cardDefined = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    colorDefined = ['S', 'H', 'D', 'C']
+
+    for i in card:
+        if (i[0:-1] not in cardDefined or i[-1] not in colorDefined):
+            print("Error input")
+            return 1
+        
+    return 0
+
+def checkStraight(card):
+    specialRule = [[1, 10, 11, 12, 13], [1, 2, 11, 12, 13], [1, 2, 3, 12, 13], [1, 2, 3, 4, 13]]
+    decode = ['A', 1, 'J', 11, 'Q', 12, 'K', 13]
+    
+    ok = 1
+    newCard = list()
+
+    for i in range(0, 5):
+        if (card[i][0:-1] in decode):
+            newCard.append(decode[decode.index(card[i][0:-1])+1])
+        else:
+            newCard.append(int(card[i][0:-1]))
+
+    newCard = sorted(newCard)
+
+    if (newCard in specialRule):
+        return 1
+
+    for i in range(1, 5):
+        if (newCard[i] != newCard[i-1] + 1):
+            ok = 0
+
+    return ok
+
+
+def getNumber(card):
+    get = 1
+
+    numberCard = [i[0:-1] for i in card]
+    setNumberCard = set(numberCard)
+    countNumber = max(numberCard, key=numberCard.count)
+
+    colorCard = [i[-1] for i in card]
+    countColor = max(colorCard, key=colorCard.count)
+
+    if (numberCard.count(countNumber) == 2):
+        get = 2
+
+    if (len(setNumberCard) == 3):
+        get = 3
+
+    if (numberCard.count(countNumber) == 3):
+        get = 4
+
+    if (checkStraight(card)):
+        get = 5
+
+    if (colorCard.count(countColor) == 5):
+        get = 6
+
+    if (len(setNumberCard) == 2):
+        get = 7
+
+    if (numberCard.count(countNumber) == 4):
+        get = 8
+
+    if (colorCard.count(countColor) == 5 and checkStraight(card)):
+        get = 9
+
+    return get
+
+def main():
+    card = input().split(' ')
+
+    sameCard = set(card)
+
+    error = 0
+
+    error = checkError(card)
+
+    if (len(card) != len(sameCard)):
+        if (error == 0):
+            print("Duplicate deal")
+            error = 1
+    
+    if (error == 0):
+        print(getNumber(card))
+
+main()
